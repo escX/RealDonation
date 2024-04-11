0fdf5657aa2404f2288f78c6a5e632d12f0258e5
escx <1768810189@qq.com>
2024-04-11 23:37:16 +0800

    完成合约功能

--- contracts/RealDonation.sol
+++ contracts/RealDonation.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
+pragma solidity ^0.8.0;
 
 // -------------------- Event --------------------
 event Create(
     bytes32 indexed projectHash, /** 项目哈希 */
-    address indexed creator,     /** 创建者 */
+    address indexed creator,     /** 创建者地址 */
     string projectName,          /** 项目名称 */
     string projectDescription,   /** 项目描述 */
     uint256 createTime           /** 创建时间 */
@@ -12,7 +12,7 @@ event Create(
 
 event ModifyDescription(
     bytes32 indexed projectHash, /** 项目哈希 */
-    string projectDescription,   /** 项目描述 */
+    string projectDescription,   /** 修改后的项目描述 */
     uint256 modifyTime           /** 修改时间 */
 );
 
@@ -34,43 +34,56 @@ event Donate(
 // -------------------- Error --------------------
 // 非法调用者
 error IllegalCaller(address caller);
-
-// 非法参数值
-error IllegalArgumentString(string data);
-error IllegalArgumentAddress(address data);
-error IllegalArgumentUint26(uint256 data);
+// 项目不存在
+error ProjectNotExist(bytes32 hash);
+// 错误的参数格式
+error IncorrectStringFormat(string str);
+// 捐赠金额不足
+error InsufficientFunds(uint256 amount);
+// 交易失败
+error TransactionFailed();
 
 
 // -------------------- Contract --------------------
 contract RealDonation {
     // -------------------- Modifier --------------------
     // 调用者必须是项目创建者
-    modifier onlyCreator(bytes32 hash) {
-        require(_project[hash].creator == msg.sender, "IllegalCaller");
+    modifier onlyCreator(bytes32 hash, address caller) {
+        if (_project[hash].creator != caller) {
+            revert IllegalCaller(caller);
+        }
         _;
     }
 
     // 调用者不能是项目创建者
-    modifier notCreator(address creator) {
-        require(creator != msg.sender, "IllegalCaller");
+    modifier notCreator(bytes32 hash, address caller) {
+        if (_project[hash].creator == caller) {
+            revert IllegalCaller(caller);
+        }
         _;
     }
 
     // 项目必须存在
     modifier projectExist(bytes32 hash) {
-        require(_project[hash].creator != address(0), "IllegalArgument");
+        if (_project[hash].creator == address(0)) {
+            revert ProjectNotExist(hash);
+        }
         _;
     }
 
     // 字符串长度限制
-    modifier validString(string calldata data, uint256 min, uint256 max) {
-        require(bytes(data).length >= min && bytes(data).length <= max, "IllegalArgument");
+    modifier validString(string calldata str, uint256 min, uint256 max) {
+        if (bytes(str).length < min || bytes(str).length > max) {
+            revert IncorrectStringFormat(str);
+        }
         _;
     }
 
     // 捐赠金额必须大于 0
-    modifier validAmount(uint256 data) {
-        require(data > 0, "IllegalArgument");
+    modifier validAmount(uint256 amount) {
+        if (amount <= 0) {
+            revert InsufficientFunds(amount);
+        }
         _;
     }
 
@@ -81,7 +94,7 @@ contract RealDonation {
      * 2、创建者地址：调用 `Create` 方法的地址，捐赠金额将直接转到该地址
      * 3、项目名称：项目的名称
      * 4、创建时间：调用 `Create` 方法的区块时间戳
-     * @notice 创建项目时，还可以添加项目描述，项目描述保存于日志中，还可以可通过 `modifyDescription` 修改
+     * @notice 创建项目时，还可以添加项目描述，项目描述保存于日志中，还可以通过 `modifyDescription` 修改
      */
     struct Project {
         bytes32 hash;
@@ -97,7 +110,7 @@ contract RealDonation {
     mapping(bytes32 hash => Project) private _project;
 
     /**
-     * @dev 私有映射变量：捐赠者 => 项目哈希 => 捐赠金额
+     * @dev 私有映射变量：捐赠者地址 => 项目哈希 => 捐赠金额
      */
     mapping(address donator => mapping(bytes32 hash => uint256)) _donated;
 
@@ -156,7 +169,7 @@ contract RealDonation {
     function create(
         string calldata name,
         string calldata description
-    ) external {
+    ) external validString(name, 1, 64) validString(description, 0, 1024) {
         bytes32 hash = keccak256(abi.encode(msg.sender, name, block.timestamp));
         _setProject(hash, msg.sender, name);
 
@@ -172,18 +185,18 @@ contract RealDonation {
      * @custom:verify 调用者必须是项目创建者
      * @custom:verify 描述长度需要符合要求
      */
-    function modifyDescription(bytes32 hash, string calldata description) external {
+    function modifyDescription(bytes32 hash, string calldata description) external onlyCreator(hash, msg.sender) validString(description, 0, 1024) {
         emit ModifyDescription(hash, description, block.timestamp);
     }
 
     /**
-     * @dev 终止项目
+     * @dev 终止项目，终止后无法再对项目进行捐赠
      * @param hash 项目哈希
      *
      * @custom:event `Cease`
      * @custom:verify 调用者必须是项目创建者
      */
-    function cease(bytes32 hash) external {
+    function cease(bytes32 hash) external onlyCreator(hash, msg.sender) {
         delete _project[hash];
 
         emit Cease(hash, block.timestamp);
@@ -196,17 +209,19 @@ contract RealDonation {
      *
      * @custom:event `Donate`
      * @custom:verify 项目必须存在
+     * @custom:verify 捐赠者地址不能为项目创建者地址
      * @custom:verify 捐赠金额必须大于 0
      * @custom:verify 捐赠留言长度需要符合要求
-     * @custom:verify 捐赠者地址不能为项目创建者地址
      */
-    function donate(bytes32 hash, string calldata message) external payable {
+    function donate(bytes32 hash, string calldata message) external payable projectExist(hash) notCreator(hash, msg.sender) validAmount(msg.value) validString(message, 0, 256) {
         Project memory project = _project[hash];
         address receiver = project.creator;
         string memory projectName = project.name;
 
         (bool success, ) = receiver.call{value: msg.value}("");
-        require(success, "Call failed");
+        if (!success) {
+            revert TransactionFailed();
+        }
 
         _setDonated(msg.sender, hash, msg.value);
 

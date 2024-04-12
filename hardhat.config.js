d2c6ea32f14a5356e8ac4d5a93f7201e6c88e202
escx <1768810189@qq.com>
2024-04-12 22:26:15 +0800

    完成测试代码

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,7 @@ require("@nomicfoundation/hardhat-chai-matchers");
 require("@nomicfoundation/hardhat-ignition-ethers");
 require("@nomicfoundation/hardhat-verify");
 require("hardhat-gas-reporter");
+require('solidity-coverage');
 require("dotenv").config();
 
 const { REPORT_GAS } = process.env;
--- test/RealDonation.test.js
+++ test/RealDonation.test.js
@@ -5,6 +5,7 @@ const { watchActionAsync } = require("./helper")
 
 const strLen0 = ''
 const strLen65 = 'a'.repeat(65)
+const strLen257 = 'a'.repeat(257)
 const strLen1025 = 'a'.repeat(1025)
 
 const strName = 'Project Name'
@@ -42,9 +43,9 @@ async function createFixtrue() {
   return { contract, creator, donator, others, projectHash }
 }
 
-describe("RealDonation Contract", function() {
-  describe("getProject", function() {
-    it("根据项目哈希，获取正确的项目名称和创建者", async function() {
+describe("RealDonation Contract", function () {
+  describe("getProject", function () {
+    it("根据项目哈希，获取正确的项目名称和创建者", async function () {
       const { contract, creator, projectHash } = await loadFixture(createFixtrue)
 
       const project = await contract.getProject(projectHash)
@@ -54,14 +55,20 @@ describe("RealDonation Contract", function() {
     })
   })
 
-  describe("getDonated", function() {
-    it("对同一个项目进行两次捐赠，获取到捐赠总额等于两次捐赠之和", async function() {
+  describe("getDonated", function () {
+    it("对同一个项目进行两次捐赠，获取到捐赠总额等于两次捐赠之和", async function () {
+      const { contract, donator, projectHash } = await loadFixture(createFixtrue)
 
+      await contract.connect(donator).donate(projectHash, strMessage, { value: donateAmount1 })
+      await contract.connect(donator).donate(projectHash, strMessage, { value: donateAmount2 })
+
+      expect(await contract.getDonated(donator.address, projectHash))
+        .to.be.equal(donateAmount1 + donateAmount2)
     })
   })
 
-  describe("create", function() {
-    it("校验项目名称长度1-64字节", async function() {
+  describe("create", function () {
+    it("校验项目名称长度1-64字节", async function () {
       const { contract, creator } = await loadFixture(deployFixture)
 
       await expect(contract.connect(creator).create(strLen0, strDescription))
@@ -73,7 +80,7 @@ describe("RealDonation Contract", function() {
         .withArgs(strLen65)
     })
 
-    it("校验项目描述长度0-1024字节", async function() {
+    it("校验项目描述长度0-1024字节", async function () {
       const { contract, creator } = await loadFixture(deployFixture)
 
       await expect(contract.connect(creator).create(strName, strLen1025))
@@ -81,7 +88,7 @@ describe("RealDonation Contract", function() {
         .withArgs(strLen1025)
     })
 
-    it("触发事件`Create`", async function() {
+    it("触发事件`Create`", async function () {
       const { contract, creator } = await loadFixture(deployFixture)
 
       await expect(contract.connect(creator).create(strName, strDescription))
@@ -90,8 +97,8 @@ describe("RealDonation Contract", function() {
     })
   })
 
-  describe("modifyDescription", function() {
-    it("校验调用者应该是项目创建者", async function() {
+  describe("modifyDescription", function () {
+    it("校验调用者应该是项目创建者", async function () {
       const { contract, others, projectHash } = await loadFixture(createFixtrue)
 
       await expect(contract.connect(others[0]).modifyDescription(projectHash, strDescriptionModified))
@@ -99,7 +106,7 @@ describe("RealDonation Contract", function() {
         .withArgs(others[0].address)
     })
 
-    it("校验项目描述长度0-1024字节", async function() {
+    it("校验项目描述长度0-1024字节", async function () {
       const { contract, creator, projectHash } = await loadFixture(createFixtrue)
 
       await expect(contract.connect(creator).modifyDescription(projectHash, strLen1025))
@@ -107,7 +114,7 @@ describe("RealDonation Contract", function() {
         .withArgs(strLen1025)
     })
 
-    it("触发事件`ModifyDescription`", async function() {
+    it("触发事件`ModifyDescription`", async function () {
       const { contract, creator, projectHash } = await loadFixture(createFixtrue)
 
       await expect(contract.connect(creator).modifyDescription(projectHash, strDescriptionModified))
@@ -116,8 +123,8 @@ describe("RealDonation Contract", function() {
     })
   })
 
-  describe("cease", function() {
-    it("校验调用者应该是项目创建者", async function() {
+  describe("cease", function () {
+    it("校验调用者应该是项目创建者", async function () {
       const { contract, others, projectHash } = await loadFixture(createFixtrue)
 
       await expect(contract.connect(others[0]).cease(projectHash))
@@ -125,7 +132,7 @@ describe("RealDonation Contract", function() {
         .withArgs(others[0].address)
     })
 
-    it("终止项目后，项目数据应被清空", async function() {
+    it("终止项目后，项目数据应被清空", async function () {
       const { contract, creator, projectHash } = await loadFixture(createFixtrue)
 
       await contract.connect(creator).cease(projectHash)
@@ -134,7 +141,7 @@ describe("RealDonation Contract", function() {
         .to.deep.equal([ethers.ZeroHash, ethers.ZeroAddress, "", 0])
     })
 
-    it("触发事件`Cease`", async function() {
+    it("触发事件`Cease`", async function () {
       const { contract, creator, projectHash } = await loadFixture(createFixtrue)
 
       await expect(contract.connect(creator).cease(projectHash))
@@ -143,8 +150,8 @@ describe("RealDonation Contract", function() {
     })
   })
 
-  describe("donate", function() {
-    it("校验项目存在", async function() {
+  describe("donate", function () {
+    it("校验项目存在", async function () {
       const { contract, donator } = await loadFixture(createFixtrue)
 
       await expect(contract.connect(donator).donate(ethers.ZeroHash, strMessage, { value: donateAmount1 }))
@@ -152,15 +159,15 @@ describe("RealDonation Contract", function() {
         .withArgs(ethers.ZeroHash)
     })
 
-    it("校验捐赠者不是项目创建者", async function() {
+    it("校验捐赠者不是项目创建者", async function () {
       const { contract, creator, projectHash } = await loadFixture(createFixtrue)
 
       await expect(contract.connect(creator).donate(projectHash, strMessage, { value: donateAmount1 }))
         .to.be.revertedWithCustomError(contract, "IllegalCaller")
-        .withArgs(creator)
+        .withArgs(creator.address)
     })
 
-    it("校验捐赠金额大于0", async function() {
+    it("校验捐赠金额大于0", async function () {
       const { contract, donator, projectHash } = await loadFixture(createFixtrue)
 
       await expect(contract.connect(donator).donate(projectHash, strMessage, { value: AmountZero }))
@@ -168,16 +175,28 @@ describe("RealDonation Contract", function() {
         .withArgs(AmountZero)
     })
 
-    it("校验留言长度0-256字节", async function() {
+    it("校验留言长度0-256字节", async function () {
+      const { contract, donator, projectHash } = await loadFixture(createFixtrue)
 
+      await expect(contract.connect(donator).donate(projectHash, strLen257, { value: donateAmount1 }))
+        .to.be.revertedWithCustomError(contract, "IncorrectStringFormat")
+        .withArgs(strLen257)
     })
 
-    it("捐赠后项目创建者余额增加", async function() {
+    it("捐赠后项目创建者余额增加", async function () {
+      const { contract, creator, donator, projectHash } = await loadFixture(createFixtrue)
 
+      await expect(contract.connect(donator).donate(projectHash, strMessage, { value: donateAmount1 }))
+        .to.changeEtherBalances([donator.address, creator.address], [-donateAmount1, donateAmount1])
     })
 
-    it("触发事件`Donate`", async function() {
+    it("触发事件`Donate`", async function () {
+      const { contract, donator, projectHash } = await loadFixture(createFixtrue)
+      const project = await contract.getProject(projectHash)
 
+      await expect(contract.connect(donator).donate(projectHash, strMessage, { value: donateAmount1 }))
+        .to.emit(contract, "Donate")
+        .withArgs(projectHash, donator.address, project[1], project[2], donateAmount1, strMessage, anyValue)
     })
   })
 })
